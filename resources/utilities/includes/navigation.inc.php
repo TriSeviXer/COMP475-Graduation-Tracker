820b4736d4b1505114eff58abdc88e8b9af24725
ErickHoy11 <124842751+ErickHoy11@users.noreply.github.com>
2023-04-29 14:26:58 -0400

    Update navigation.inc.php

--- resources/utilities/includes/navigation.inc.php
+++ resources/utilities/includes/navigation.inc.php
@@ -2,12 +2,12 @@
     <a href="/COMP475-Graduation-Tracker/web-pages/index.php">Home</a>
     <a href="/COMP475-Graduation-Tracker/web-pages/about.php">About</a>
     <?php
-        if (isset($_SESSION["usersId"])){
-            echo '<a href="/COMP475-Graduation-Tracker/web-pages/account/course-selection.php">Main Page</a> ';
+        if (isset($_SESSION["user_id"])){
+            echo '<a href="/COMP475-Graduation-Tracker/web-pages/account/course-selection.php">Main Page</a>';
             echo '<a href="/COMP475-Graduation-Tracker/resources/utilities/includes/logout.inc.php">Log Out</a>';
         }
         else{
-            echo '<a href="/COMP475-Graduation-Tracker/web-pages/login/login.php">Login</a> ';
+            echo '<a href="/COMP475-Graduation-Tracker/web-pages/login/login.php">Login</a>';
             echo '<a class="positive" href="/COMP475-Graduation-Tracker/web-pages/login/sign-up.php">Sign-up</a>';
         }
     ?>
