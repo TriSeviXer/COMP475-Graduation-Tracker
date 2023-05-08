8db775ab7f119b921dd65b4519f41c2b4a97e842
TriSeviXer <gameingwarlord@gmail.com>
2023-05-08 17:43:00 -0400

    About Page Updates

--- web-pages/about.php
+++ web-pages/about.php
@@ -20,24 +20,36 @@
 		</header>
         <main class="selection">
             <section class="container">
-                <h1>Commonwealth University Tracker</h1>
-                <p>
-                    Welcome to our Graduation Tracker! This handy tool will 
-                    automatically track which classes are required to be taken for your given major. 
-                    It will also list the prerequistes for each class as well.
-                    Sign up or Login to get started!
-                </p>
+                <h1>Graduation Tracker</h1>
+                <section>
+                    <h2>Our Mission</h2>
+                    <p>
+                        Welcome to our Graduation Tracker! This handy tool will 
+                        automatically track which classes are required to be taken for your given major. 
+                        It will also list the prerequistes for each class as well.
+                        Sign up or Login to get started!
+                    </p>
+                    <h2>Features</h2>
+                    <ul>
+                        <li>Tracking Majors</li>
+                        <li>Creating Schedules</li>
+                        <li>Adding and Removing Courses</li>
+                    </ul>
+                </section>
+                
             </section>
             <section class="container">
                 <h1>Meet The Team</h1>
-                <h2>Austin </h2>
-                <p>Database<p>
-                <h2>Erick</h2>
-                <p>Back End<p>
-                <h2>Michael</h2>
-                <p>Front End<p>
-                <h2>Zach </h2>
-                <p>Documentation<p>
+                <section>
+                    <h2>Austin </h2>
+                    <p>Database Management<p>
+                    <h2>Erick</h2>
+                    <p>Back End<p>
+                    <h2>Michael</h2>
+                    <p>Front End & Version Control<p>
+                    <h2>Zach </h2>
+                    <p>Documentation<p>
+                </section>
             </section>
 		</main>
         <footer>
