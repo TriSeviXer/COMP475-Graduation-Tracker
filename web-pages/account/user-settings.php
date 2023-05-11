18a7038336c87cbbc17310eb9b60ce51f3e1a172
ErickHoy11 <124842751+ErickHoy11@users.noreply.github.com>
2023-05-10 20:21:11 -0400

    Create user-settings.php

---
+++ web-pages/account/user-settings.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Graduation Tracker</title>
+        <link rel="Stylesheet" type="text/css" href="../../resources/utilities/styles/grad-tracker.css">
+        <script src="../../resources/utilities/scripts/grad-script.js" defer></script>
+    </head>
+    <body>
+        <?php
+            include '../../resources/utilities/includes/navigation.inc.php';
+        ?>
+        <header>
+            <h1>Profile Settings</h1>
+        </header>
+        <main class="selection">
+            <section class="container">
+                <h1>MY SETTINGS</h1>
+                <p>Some settings may not be available yet.</p>
+                <button>Change Profile Image</button>
+                <button>Change Email</button>
+                <button>Change Password</button>
+                <button>Delete Account</button>
+            </section>
+    </body>
+<html>
